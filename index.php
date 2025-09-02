f84b3aff6e8a28109040a488fbe9a7b4c9066653
GertenP <gerten.pilv@tptlive.ee>
2025-09-02 11:31:46 +0300

    array

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php
 
-echo "Hello World!\n"; 
+$array = array(1, 2, 3, 4, 5);
+$array = [1, 2, 3, 4, 5, 6];
 
-$var = 1;
+$array = [
+    'name' => 'Gerten',
+    'age' => 18,
+    'gender' => 'male',
+    'isCool' => TRUE
+];
 
-echo $var . "Tere\n";
-    
-var_dump($var); // int(1)
+$array = [
+    6 => 'lol',
+    1,
+    2,
+    1 => 3,
+    'name' => 'Gerten',
+    'age' => 18,
+    'gender' => 'male',
+    'isCool' => TRUE,
+    4,
+    5
+];
 
- 
+
+
+var_dump($array['isCool']); // int(1)
+
+$array = [1, 2, 3];
+array_push($array, 4);
+
+$array[] = 5;
+var_dump($array);
